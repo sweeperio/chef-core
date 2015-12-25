ccca06e8bbb8761b212b5632e7f37e71a3c5244d
pseudomuto <david.muto@gmail.com>
2015-12-25 16:10:12 -0500

    requiring libraries in metadata

--- metadata.rb
+++ metadata.rb
@@ -7,3 +7,5 @@ description      "Common resources and libraries for chef"
 long_description "Common resources and libraries for chef"
 version          "0.1.0"
 # rubocop:enable Style/SingleSpaceBeforeFirstArg
+
+require File.expand_path("../libraries/chef", __FILE__)
